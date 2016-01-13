a81cdb7d7bb59c56c37a5f38f1a2ae3cb316db88
Jerry Zheng <jiaruizheng0714@gmail.com>
2016-01-13 23:25:37 +0000

    rockpaperscissors

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,20 +1,47 @@
 var userPoint = 0;
 var aiPoint = 0;
 
-// This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+  var number = Math.floor((Math.random() * (3)));
+  var array = ['rock','paper','scissors'];
+   return array[number];
 }
 
-// This function picks the winner
-function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
-    //TODO: Add one point for the winner
+
+var aiValue = getAISelection();
+
+function pickWinner(userValue,aiValue){
+   if (userValue === aiValue){
+      console.log('draw');
+      return 'draw';
+    } else if (userValue =='rock' && aiValue =='paper'){
+      console.log('ai');
+      aiPoint ++;
+    }else if (userValue =='rock' && aiValue =='scissors'){
+      console.log('user'); 
+      userPoint ++;
+      return 'user';
+    }else if (userValue =='scissors' && aiValue =='paper'){
+      console.log('user'); 
+      userPoint ++;
+      return 'user';
+    }else if (userValue =='scissors' && aiValue =='rock'){
+      console.log('ai');
+      aiPoint ++;
+    }else if (userValue =='paper' && aiValue =='rock'){
+      console.log('user'); 
+      userPoint  ++;
+      return 'user';
+    }else if (userValue =='paper' && aiValue =='scissors'){
+      console.log('ai');
+      aiPoint ++;
+    }
 }
 
 // This function sets the scoreboard with the correct points
 function setScore() {
-
+    $('#userPoint').text(userPoint);
+    $('#aiPoint').text(aiPoint);
 }
 
 // This function captures the click and picks the winner
@@ -25,15 +52,25 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
-        $('#message').delay(50).text('You have won!, Click a box to play again');
+        $('#message').delay(50).text('You have won! You selected '+ userValue + ' and the computer selected '+aiValue+'. Click a box to play again');
+        $('#userPoint').text(userPoint);
+        $('#aiPoint').text(aiPoint);
     } else if ( winner === 'draw' ) {
-        $('#message').delay(50).text('Draw! Click a box to play again');
+        $('#message').delay(50).text('Draw! You selected '+ userValue+ ' and the computer selected '+aiValue+'. Click a box to play again');
+        $('#userPoint').text(userPoint);
+        $('#aiPoint').text(aiPoint);
     } else {
-        $('#message').delay(50).text('You have lost!, Click a box to play again');
+        $('#message').delay(50).text('You have lost! You selected '+userValue+ ' and the computer selected '+aiValue+'. Click a box to play again');
+        $('#userPoint').text(userPoint);
+        $('#aiPoint').text(aiPoint);
     }
 }
 
 // This function runs on page load
 $(document).ready(function(){
+  $( '.token' ).click(function (evt) {
+  evaluate(evt);
+  
+});});
 
-});
+
